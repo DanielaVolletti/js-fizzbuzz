ebe7d503e599787f3906ae59faa3fad96b98aea5
Daniela Maria Volletti <danielavolletti@gmail.com>
2020-03-26 12:09:45 +0100

    creazione output in pagina

--- bonus/js/main.js
+++ bonus/js/main.js
@@ -6,7 +6,7 @@ Per i numeri che sono sia multipli di 3 che di 5 stampi” FizzBuzz”.*/
 
 
 // dichiaro le variabili dei multipli
-var multiploTre, multiploCinque, multiploTreCinque;
+var multiploTre, multiploCinque, multiploTreCinque, output;
 
 // creo un ciclo for di numeri da 1 a 100
 for (var i = 1; i <= 100; i++) {
@@ -19,12 +19,22 @@ for (var i = 1; i <= 100; i++) {
   // creo condizionali per trovare i multipli e li stampo
   if (multiploTreCinque) {
     console.log("FizzBuzz");
+    output = "FizzBuzz";
   } else if (multiploTre) {
     console.log("Fizz");
+    output = "Fizz";
   } else if (multiploCinque) {
     console.log("Buzz");
+    output = "Buzz";
   } else {
     console.log(i);
+    output = i;
   }
+
+  // creo una variabile per conservare elementi ciclo
+  var outputPrec = document.getElementById('fizz-buzz').innerHTML;
+
+  // output in pagina
+  document.getElementById('fizz-buzz').innerHTML = outputPrec + "<li>" + output + "</li>";
 
 }
