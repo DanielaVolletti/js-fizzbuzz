f4dbceeca3750a3b99674becb6d3dd842dd0c431
Daniela Maria Volletti <danielavolletti@gmail.com>
2020-03-26 10:56:45 +0100

    inizio a stampare i multipli

--- js/main.js
+++ js/main.js
@@ -6,6 +6,28 @@ Per i numeri che sono sia multipli di 3 che di 5 stampi” FizzBuzz”.*/
 
 
 // dichiaro le variabili dei multipli
-
+var multiploTre, multiploCinque, multiploTreCinque;
 
 // creo un ciclo for di numeri da 1 a 100 con condizionali per i diversi multipli
+
+for (var i = 1; i <= 100; i++) {
+
+  // assegno un valore ai multipli
+  multiploTre = i / 3;
+
+  multiploCinque = i / 5;
+
+  multiploTreCinque = multiploTre && multiploCinque;
+
+  // trovo i multipli di tre e li stampo
+  if (multiploTre == parseInt(multiploTre)) {
+    console.log("Fizz");
+  } else if (multiploTreCinque == parseInt(multiploTreCinque)) {
+    console.log("FizzBuzz");
+  } else {
+    console.log(i);
+  }
+
+
+
+}
