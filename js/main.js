e3a31057360f6f0af97f398f9e7622fa7de476e0
Daniela Maria Volletti <danielavolletti@gmail.com>
2020-03-26 11:17:07 +0100

    multipli trovati e in console log

--- js/main.js
+++ js/main.js
@@ -9,25 +9,24 @@ Per i numeri che sono sia multipli di 3 che di 5 stampi” FizzBuzz”.*/
 var multiploTre, multiploCinque, multiploTreCinque;
 
 // creo un ciclo for di numeri da 1 a 100 con condizionali per i diversi multipli
-
 for (var i = 1; i <= 100; i++) {
 
   // assegno un valore ai multipli
-  multiploTre = i / 3;
-
-  multiploCinque = i / 5;
-
+  multiploTre = (i % 3 == 0);
+  multiploCinque = (i % 5 == 0);
   multiploTreCinque = multiploTre && multiploCinque;
 
-  // trovo i multipli di tre e li stampo
-  if (multiploTre == parseInt(multiploTre)) {
+  // trovo i multipli e li stampo
+  if (multiploTre) {
     console.log("Fizz");
-  } else if (multiploTreCinque == parseInt(multiploTreCinque)) {
-    console.log("FizzBuzz");
+  } else if (multiploCinque) {
+    console.log("Buzz");
   } else {
     console.log(i);
   }
 
-
+  if (multiploTreCinque) {
+    console.log("FizzBuzz");
+  }
 
 }
